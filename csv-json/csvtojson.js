1a031d2d5e8c86b5167134ae1340f3b886165b5d
Vikram-Sehgal <66684840+Vikram-Sehgal@users.noreply.github.com>
2020-06-10 11:34:23 +0530

    Add files via upload

---
+++ csv-json/csvtojson.js
@@ -0,0 +1,46 @@
+// My own code to convert a csv file to json file
+
+const fs = require('fs');
+const readline = require('readline');
+const file = fs.createReadStream('customer-data.xls');
+const rl = readline.createInterface(file);
+
+let buff = [];
+let i = 0;
+rl.on('line', (line) => {
+	buff[i] = line.split(',');
+	i++;
+	0;
+});
+
+rl.on('close', () => {
+	let l = 1;
+	let my = {};
+	let arr = []; //
+
+	for (let j = 0; j < buff.length - 1; j++) {
+		for (let k = 0; k < buff[0].length; k++) {
+			my[buff[0][k]] = buff[l][k];
+		}
+		arr[l - 1] = JSON.stringify(my);
+		l++;
+	}
+
+	fs.writeFile('customer-data.json', '[' + arr + ']', (e) => {
+		if (e) console.error('got error : ' + e);
+		console.log('Your csv file converted to json.');
+	});
+});
+
+// convert a csv file to json file with csvtojson module
+
+// const fs = require('fs');
+// const csv = require('csvtojson');
+// const cws = fs.createWriteStream('customer-data.json');
+
+// csv().fromFile('customer-data.xls').then((jsonObj) => {
+// 	cws.write(JSON.stringify(jsonObj), (error) => {
+// 		if (error) console.error('Got Errror : ' + error);
+// 		console.log('Your csv file converted to json.');
+// 	});
+// });
